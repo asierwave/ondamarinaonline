c9cf20c014367adc3d23a92c912075c19a08d153
asierwave <asier.toral@gmail.com>
2024-09-28 21:18:38 +0200

    Update xlsblogpush.js

--- Assets/scripts/xlsblogpush.js
+++ Assets/scripts/xlsblogpush.js
@@ -1,8 +1,8 @@
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxJW8hIoLUG7GGnkTPaDhfMqX08Zwqb9IIPXJrEuvU3cdkcLqRcQrYoTzTP88IViS8gKQ/exec'
-﻿
+
 const form = document.forms['contact-form']
-﻿
+
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
