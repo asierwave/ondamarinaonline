7b0211118200c2baee390ee1b7da765c010fe06a
asierwave <asier.toral@gmail.com>
2024-09-29 15:34:52 +0200

    CAMBIOS

--- Assets/scripts/xlsblogpush.js
+++ Assets/scripts/xlsblogpush.js
@@ -3,8 +3,6 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxwinYrKxgx2FTQ8J5kgM
 
 const form = document.forms['contact-form']
 
-
-
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
