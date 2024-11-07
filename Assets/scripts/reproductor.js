76d812ca863aad451be46803e0d151d11c89bcd5
asierwave <125518587+asierwave@users.noreply.github.com>
2024-11-07 23:00:00 +0100

    v4

--- Assets/scripts/reproductor.js
+++ Assets/scripts/reproductor.js
@@ -1,77 +1,79 @@
-const miAudio = new Audio();
-let defaultStreamURL = 'https://servidor40.brlogic.com:7004/live';
-miAudio.src = defaultStreamURL;
-
-const iconosPlay = document.querySelectorAll('.iconoplay');
-iconosPlay.forEach(iconoPlay => {
-  iconoPlay.addEventListener('click', function () {
-    const streamURL = this.closest('.programareciente').dataset.streamurl || defaultStreamURL;
-    miAudio.src = streamURL;
-
-    fetch(streamURL)
-      .then(response => response.text())
-      .then(html => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-
-        const textoPodcast = doc.getElementById('streamtitle').textContent.trim();
-        const descripcionPodcast = doc.getElementById('streamdescription').textContent.trim();
-
-        console.log('Texto del podcast:', textoPodcast);
-        console.log('Descripción del podcast:', descripcionPodcast);
-
-        miAudio.play();
-      })
-      .catch(error => {
-        console.error('Error al obtener información del podcast:', error.message);
-      });
+// Crea un objeto Howl para el audio
+// Crea un objeto Howl para el audio
+let miAudio = new Howl({
+  src: ["https://servidor40.brlogic.com:7004/live?identifier=ondamarina&source=7621"], // Nueva URL del archivo de audio
+  autoplay: false, // No reproducir automáticamente al cargar la página
+  loop: false, // No reproducir en bucle
+  volume: 1.0 // Volumen del audio (0 a 1)
+});
+  
+  // Obtiene una referencia al botón de imagen por su ID
+  let botonFotoplay = document.getElementById('imagen-reproducir-pausar');
+  let botonTreintaAtras = document.getElementById ('imagen-treint-atras');
+  let botonTreintaAdelante = document.getElementById ('imagen-treint-adelante');
+  
+  // Variable para realizar un seguimiento del estado de reproducción
+  let estaReproduciendo = false;
+  
+  // Agrega un evento click al botón de imagen para controlar la reproducción/pausa
+  botonFotoplay.addEventListener('click', function() {
+    if (estaReproduciendo) {
+      miAudio.pause(); // Pausa el audio si está reproduciéndose
+      botonFotoplay.setAttribute('src', 'Assets/adelante.png'); // Cambia la imagen a "Reproducir"
+      estaReproduciendo = false;
+    } else {
+      miAudio.play(); // Reproduce el audio si está pausado
+      botonFotoplay.setAttribute('src', 'Assets/pause.png'); // Cambia la imagen a "Pausar"
+      estaReproduciendo = true;
+    }
   });
-});
-
-const botonFotoplay = document.getElementById('imagen-reproducir-pausar');
-let estaReproduciendo = false;
-
-botonFotoplay.addEventListener('click', function () {
-  if (miAudio.paused) {
-    miAudio.play();
-    estaReproduciendo = true;
-  } else {
-    miAudio.pause();
+  
+  // Evento para detectar cuando se termina la reproducción
+  miAudio.on('end', function() {
+    botonFotoplay.setAttribute('src', 'Assets/adelante.png'); // Cambia la imagen a "Reproducir" cuando el audio finaliza
     estaReproduciendo = false;
+  });
+  
+
+// BOTÓN DE RETROCESO 30 SEGUNDOS
+
+// Agrega un manejador de eventos al botón
+botonTreintaAtras.addEventListener("click", retroceder30Segundos);
+
+// Función para retroceder 30 segundos
+function retroceder30Segundos() {
+  var currentPosition = miAudio.seek(); // Obtiene la posición actual
+  var newPosition = currentPosition - 10; // Retrocede 10 segundos
+
+  // Asegúrate de que la nueva posición no sea menor que 0
+  if (newPosition < 0) {
+    newPosition = 0;
   }
-  actualizarEstadoReproduccion(estaReproduciendo); // Actualiza el estado de reproducción
-});
 
-window.addEventListener('scroll', function () {
-  actualizarEstadoReproduccion(estaReproduciendo);
-});
-
-const botonDiezSegundosAtras = document.getElementById('imagen-diez-segundos-atras');
-botonDiezSegundosAtras.addEventListener('click', function() {
-  miAudio.currentTime -= 10;
-});
-
-const botonEnVivo = document.getElementById('imagen-en-vivo');
-botonEnVivo.addEventListener('click', function() {
-  miAudio.currentTime = lastBufferedTime;
-  miAudio.play();
-});
-
-let lastBufferedTime = 0;
-miAudio.addEventListener('progress', function () {
-  if (miAudio.buffered.length > 0) {
-    lastBufferedTime = miAudio.buffered.end(miAudio.buffered.length - 1);
-  }
-});
-
-miAudio.addEventListener('timeupdate', function () {
-  actualizarEstadoReproduccion(!miAudio.paused);
-});
-
-function actualizarEstadoReproduccion(estaReproduciendo) {
-  if (estaReproduciendo) {
-    botonFotoplay.setAttribute('src', 'Assets/pausewhite.png');
-  } else {
-    botonFotoplay.setAttribute('src', 'Assets/playwhite.png');
-  }
+  miAudio.seek(newPosition); // Establece la nueva posición
 }
+
+
+// BOTÓN DE ADELANTO 30 SEGUNDOS
+
+
+// Agrega un manejador de eventos al botón
+botonTreintaAdelante.addEventListener("click", adelantar30Segundos);
+
+// Función para adelantar 30 segundos
+function adelantar30Segundos() {
+  var currentPosition = miAudio.seek(); // Obtiene la posición actual
+  var newPosition = currentPosition + 10; // Retrocede 10 segundos
+
+  // Asegúrate de que la nueva posición no sea mayor que 100
+  if (newPosition > 100) {
+    newPosition = 100;
+  } 
+
+  miAudio.seek(newPosition); // Establece la nueva posición
+}
+
+
+
+
+  
